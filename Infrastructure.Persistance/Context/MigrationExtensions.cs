f464af23fb5bf6cb7661dc7664b539b8c5a9529a
Mojaba Hafezi <mojtaba.hafezi.wien@gmail.com>
2024-05-14 11:40:32 +0200

    Docker Compose

---
+++ Infrastructure.Persistance/Context/MigrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.Context
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using AppDbContext dbContext = 
+                scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            if (!dbContext.Database.CanConnect())
+            {
+                dbContext.Database.Migrate();
+            }
+        }
+    }
+}
--- Infrastructure.Persistance/Extensions/ServiceRegistrations.cs
+++ Infrastructure.Persistance/Extensions/ServiceRegistrations.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistance.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,12 @@ namespace Infrastructure.Persistance.Extensions
     {
         public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration config)
         {
-            var connectionString = config.GetValue<string>("CoreMarketConnection");
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+            var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}; TrustServerCertificate=True;";
+
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
             #region Repositories
