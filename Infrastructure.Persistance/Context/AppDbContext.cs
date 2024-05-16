0635f480df4ed4824b2551235d24bae2177fb3db
Mojaba Hafezi <mojtaba.hafezi.wien@gmail.com>
2024-05-16 11:50:31 +0200

    Refactoring AppDbContext

--- Infrastructure.Persistance/Context/AppDbContext.cs
+++ Infrastructure.Persistance/Context/AppDbContext.cs
@@ -11,31 +11,12 @@ public class AppDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Brand> Brands { get; set; }
 
-    private readonly IConfiguration _configuration;
-    private readonly IHostEnvironment _hostEnvironment;
-    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration, IHostEnvironment hostEnvironment) : base(options)
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        _configuration = configuration;
-        _hostEnvironment = hostEnvironment;
+
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        //if (_hostEnvironment.IsDevelopment())
-        //{
-        //    optionsBuilder.UseSqlServer(
-        //    _configuration.GetValue<string>("CoreMarketConnection"), x => x.MigrationsHistoryTable("MigrationsHistory", "BASE")
-        //    );
-        //}
-        //else
-        //{
-        //    var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-        //    var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-        //    var connectionString = $"Server={dbHost}; Database={dbName}; Trusted_Connection=True; TrustServerCertificate=True;";
-        //    optionsBuilder.UseSqlServer(
-        //        );
-        //}
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
